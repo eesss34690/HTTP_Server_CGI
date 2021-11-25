6a8d969861de08ebf31cd16095dcf4591bcde82b
eesss34690 <eesss.ee07@nctu.edu.tw>
2021-11-25 16:51:39 +0800

    add initial test console

--- Makefile
+++ Makefile
@@ -13,8 +13,13 @@ CXX_INCLUDE_PARAMS=$(addprefix -I , $(CXX_INCLUDE_DIRS))
 CXX_LIB_DIRS=/usr/local/lib
 CXX_LIB_PARAMS=$(addprefix -L , $(CXX_LIB_DIRS))
 
-all: http_server.cpp
-	$(CXX) http_server.cpp -o http_server $(CXX_INCLUDE_PARAMS) $(CXX_LIB_PARAMS) $(CXXFLAGS)
+all: http_server console.cgi
+
+http_server: http_server.cpp
+	$(CXX) -o $@ $^ $(CXX_INCLUDE_PARAMS) $(CXX_LIB_PARAMS) $(CXXFLAGS)
+
+console.cgi: console.cpp
+	$(CXX) -o $@ $^ $(CXX_INCLUDE_PARAMS) $(CXX_LIB_PARAMS) $(CXXFLAGS)
 
 clean:
 	rm -f http_server
---
+++ console.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>  
+#include <string>  
+#include <stdio.h>  
+#include <stdlib.h>
+
+using namespace std;
+
+int main ()
+{
+    cout << "<!DOCTYPE html>\r\n\r\n";
+    cout << "<html lang=\"en\">\r\n";
+    cout << "<head>\r\n";
+    cout << "<meta charset=\"UTF-8\" />\r\n";
+    cout << "<title>NP Project 3 Sample Console</title>\r\n";
+    cout << "<link\r\n";
+    cout << "  rel=\"stylesheet\"\r\n";
+    cout << "  href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css\"\r\n";
+    cout << "  integrity=\"sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2\"\r\n";
+    cout << "  crossorigin=\"anonymous\"\r\n";
+    cout << "/>\r\n";
+    cout << "<link\r\n";
+    cout << "  href=\"https://fonts.googleapis.com/css?family=Source+Code+Pro\"\r\n";
+    cout << "  rel=\"stylesheet\"\r\n";
+    cout << "/>\r\n";
+    cout << "<link\r\n";
+    cout << "  rel=\"icon\"\r\n";
+    cout << "  type=\"image/png\"\r\n";
+    cout << "  href=\"https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png\"\r\n";
+    cout << "/>\r\n";
+    cout << "<style>\r\n";
+    cout << "  * {\r\n";
+    cout << "    font-family: 'Source Code Pro', monospace;\r\n";
+    cout << "    font-size: 1rem !important;\r\n";
+    cout << "  }\r\n";
+    cout << "  body {\r\n";
+    cout << "    background-color: #212529;\r\n";
+    cout << "  }\r\n";
+    cout << "  pre {\r\n";
+    cout << "    color: #cccccc;\r\n";
+    cout << "  }\r\n";
+    cout << "  b {\r\n";
+    cout << "    color: #01b468;\r\n";
+    cout << "  }\r\n";
+    cout << "</style>\r\n";
+    cout << "</head>";
+    cout << "  <body>\r\n";
+    cout << "<table class=\"table table-dark table-bordered\">\r\n";
+    cout << "  <thead>\r\n";
+    cout << "    <tr>\r\n";
+    cout << "      <th scope=\"col\">nplinux1.cs.nctu.edu.tw:1234</th>\r\n";
+    cout << "      <th scope=\"col\">nplinux2.cs.nctu.edu.tw:5678</th>\r\n";
+    cout << "    </tr>\r\n";
+    cout << "  </thead>\r\n";
+    cout << "  <tbody>\r\n";
+    cout << "    <tr>\r\n";
+    cout << "      <td><pre id=\"s0\" class=\"mb-0\"></pre></td>\r\n";
+    cout << "      <td><pre id=\"s1\" class=\"mb-0\"></pre></td>\r\n";
+    cout << "    </tr>\r\n";
+    cout << "  </tbody>\r\n";
+    cout << "</table>\r\n";
+    cout << "</body>\r\n";
+    cout << "</html>\r\n";
+}
